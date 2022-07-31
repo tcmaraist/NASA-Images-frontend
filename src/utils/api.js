b93497efd1db41db1344d31f15e3d74c78fd4619
Ty Maraist <tcmaraist@gmail.com>
2022-07-31 18:38:41 -0500

    push for coworking hours

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,10 +3,18 @@ class Api {
     this._baseUrl = baseUrl;
     this._headers = headers;
   }
+
+  _handleServerResponse(res) {
+    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/asset/?orderby=popular`);
+  }
 }
 
 const api = new Api({
-  baseUrl: "https://images-api.nasa.gov/",
+  baseUrl: "https://images-api.nasa.gov",
   headers: {
     Authorizaion: "KNwVoypkMlR1eO8crEcvjgcm2fOaCoejuwIyKQ8b",
     "Content-Type": "application/json",
