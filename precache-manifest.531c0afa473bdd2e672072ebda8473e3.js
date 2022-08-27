90a8a1893db2df1f31d3cc31acbc3929fd2174b6
Ty Maraist <tcmaraist@gmail.com>
2022-08-27 18:37:18 -0500

    Updates

---
+++ precache-manifest.531c0afa473bdd2e672072ebda8473e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e259e784552a9bd72907",
+    "url": "/NASA-Images-frontend/static/css/main.78fb66b2.chunk.css"
+  },
+  {
+    "revision": "e259e784552a9bd72907",
+    "url": "/NASA-Images-frontend/static/js/main.3d3ca60f.chunk.js"
+  },
+  {
+    "revision": "4bdd1cce19ecc54190ba",
+    "url": "/NASA-Images-frontend/static/js/runtime~main.e874bf2d.js"
+  },
+  {
+    "revision": "3607b8456ea30f4dd8fc",
+    "url": "/NASA-Images-frontend/static/js/2.3aa6013b.chunk.js"
+  },
+  {
+    "revision": "72c1338e0934456dbac619658327661e",
+    "url": "/NASA-Images-frontend/static/media/nasa-logo.72c1338e.svg"
+  },
+  {
+    "revision": "5e7a07dde446b4a0299df88a87902dd2",
+    "url": "/NASA-Images-frontend/static/media/card-default.5e7a07dd.jpeg"
+  },
+  {
+    "revision": "83dac2b1e5969ac6d7a686e35ed0f12d",
+    "url": "/NASA-Images-frontend/index.html"
+  }
+];
